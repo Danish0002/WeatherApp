5bea2da81d12c4635892fa8fa728ba4723f9f8ee
Danish0002 <121163878+Danish0002@users.noreply.github.com>
2025-03-30 19:58:28 +0530

    Update useWeather.js

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_KEY = process.env.VITE_OPENWEATHERMAP_API_KEY;
+const API_KEY = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
 
 
 
