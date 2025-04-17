0a8c8e7b90092fe83d6e73acf759d6348c1da992
Danish0002 <121163878+Danish0002@users.noreply.github.com>
2025-04-17 07:18:41 +0530

    Update homePage.jsx
    made it responsive for small screens

--- src/components/home/homePage.jsx
+++ src/components/home/homePage.jsx
@@ -10,16 +10,16 @@ const HomePage = () => {
         backgroundPosition: 'center',
       }}
     >
-      <div className="absolute bottom-8 right-8 z-10 backdrop-blur-md p-8 rounded-4xl border-4 border-white hover:border-blue-300 shadow-xl text-center w-full max-w-lg transform transition-all duration-500 hover:scale-105 hover:shadow-2xl">
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-10 backdrop-blur-md p-6 sm:p-8 px-4 sm:px-8 rounded-4xl border-4 border-white hover:border-blue-300 shadow-xl text-center w-full max-w-md transition-all duration-500 hover:scale-105 hover:shadow-2xl">
         <img
           src="/logo1.png"
           alt="Logo"
-          className="w-32 h-32 mb-8 rounded-full mx-auto shadow-xl border-4 border-white transition-all duration-300 hover:border-blue-500"
+          className="w-24 h-24 mb-6 rounded-full mx-auto shadow-xl border-4 border-white transition-all duration-300 hover:border-blue-500"
         />
-        <p className="text-lg text-blue-300 dark:text-gray-300 mb-6">
+        <p className="text-lg text-blue-300 dark:text-gray-300 mb-4">
           Search for a location to get the weather forecast.
         </p>
-        <p className="text-3xl font-bold text-blue-300 dark:text-gray-100">
+        <p className="text-2xl font-bold text-blue-300 dark:text-gray-100">
           Weather App by Danish
         </p>
       </div>
